4cd0a923f59deb8cfacd7923b5b034830393b8cd
abrahamve <abrahamve_01@hotmail.com>
2023-11-14 10:07:04 -0600

    Cambie el color del span a azul

--- resources/views/components/input1.blade.php
+++ resources/views/components/input1.blade.php
@@ -3,7 +3,7 @@
             <div class="col-8">
                 <input type="text" class="form-control" name="{{ $texto }}" value="{{ old($texto, $campo) }}" id="" placeholder="">
                 @error( $texto )
-                    <span style="color:red">{{ $message }}</span>
+                    <span style="color:rgb(34, 0, 255)">{{ $message }}</span>
                 @enderror
             </div>
         </div>
